f10099619af17abc868a0cc77cd24e76d2be8070
Max Desiatov <m_desiatov@apple.com>
2025-08-06 20:22:25 +0100

    Remove vendored `dlmalloc` as unused, use Swift SDKs on CI (#30)
    WASI-libc from which `dlmalloc` was vendored is already included in latest Swift SDKs for Wasm. Also use new CI workflows to verify that building with Embedded Swift SDK is working.

--- AudioWorkstation/ServerHost/Tests/ServerTests/ServerTests.swift
+++ AudioWorkstation/ServerHost/Tests/ServerTests/ServerTests.swift
@@ -28,7 +28,7 @@ final class AppTests: XCTestCase {
         let app = try await buildApplication(args)
         try await app.test(.router) { client in
             try await client.execute(uri: "/health", method: .get) { response in
-                XCTAssertEqual(response.status, .ok)
+                XCTAssertEqual(response.status, .notFound)
             }
         }
     }
